cdc23d59c2ffebd019ff55f5b2ccc56c06ec4058
Morad17 <morad123@hotmail.co.uk>
2021-10-17 20:37:47 +0100

    building homescreen

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.container}>
 
-        <View style={styles.container}>
-          
+      <View style={styles.carContainer}>
+
+      <ImageBackground source={require('./assets/images/model3.jpg')} style={styles.image} />
+
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at £70000</Text>
         </View>
 
       </View>
@@ -26,4 +30,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer: {
+
+    width: '100%',
+    height: '100%',
+  },
+  titles:{
+    margin: '30%',
+    width: '100%',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '600'
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62'
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position:'absolute',
+  }
 });
